1e446cd94d2d04ca759e5182bdc1b7d59a4c9c79
NintenHero <mchinrichs@gmail.com>
2023-10-28 23:37:26 -0400

    Update magic check
    Changed the check to a negative. change the "This file is not a NERD engine NPK file." message to a proper error. Pull the part of the code that runs if it IS given a npk file, out of the if statement.

--- Program.cs
+++ Program.cs
@@ -22,47 +22,44 @@ namespace npkExtract
             BinaryReader br = new(source);
 
             string magic = new(br.ReadChars(4));
-            if (magic == "npk." || magic == ".npk")
+            if (magic != "npk." && magic != ".npk")
             {
-                if (magic == ".npk")
-                {
-                    br.ReadBytes(4);
-                }
-                uint metaOffset = br.ReadUInt32();
-                uint metaSize = br.ReadUInt32();
-                Subfile[] subfile = new Subfile[metaSize / 0X100];
-                br.BaseStream.Position = metaOffset;
-                for (int i = 0; i < subfile.Length; i++)
-                {
-                    long nameOffset = br.BaseStream.Position;
-                    char[] fileName = Array.Empty<char>();
-                    char readChar = (char)1;
-                    while (readChar > 0)
-                    {
-                        readChar = br.ReadChar();
-                        Array.Resize(ref fileName, fileName.Length + 1);
-                        fileName[^1] = readChar;
-                    }
-                    Array.Resize(ref fileName, fileName.Length - 1);
-                    subfile[i].Name = new(fileName);
-                    br.BaseStream.Position = nameOffset + 0XF8;
-                    subfile[i].Offset = br.ReadUInt32();
-                    subfile[i].Size = br.ReadUInt32();
-                }
-
-                for (int i = 0; i < subfile.Length; i++)
-                {
-                    Directory.CreateDirectory(Path.GetDirectoryName(source.Name) + "\\" + Path.GetFileNameWithoutExtension(source.Name));
-                    BinaryWriter bw = new(File.OpenWrite(Path.GetDirectoryName(source.Name) + "\\" + Path.GetFileNameWithoutExtension(source.Name) + "\\" + subfile[i].Name));
-                    br.BaseStream.Position = subfile[i].Offset;
-                    bw.Write(br.ReadBytes((int)subfile[i].Size));
-
-                    bw.Close();
-                }
+                throw new ArgumentException("This file is not a NERD engine NPK file.");
             }
-            else
+            if (magic == ".npk")
             {
-                Console.WriteLine("This file is not a NERD engine NPK file.");
+                br.ReadBytes(4);
+            }
+            uint metaOffset = br.ReadUInt32();
+            uint metaSize = br.ReadUInt32();
+            Subfile[] subfile = new Subfile[metaSize / 0X100];
+            br.BaseStream.Position = metaOffset;
+            for (int i = 0; i < subfile.Length; i++)
+            {
+                long nameOffset = br.BaseStream.Position;
+                char[] fileName = Array.Empty<char>();
+                char readChar = (char)1;
+                while (readChar > 0)
+                {
+                    readChar = br.ReadChar();
+                    Array.Resize(ref fileName, fileName.Length + 1);
+                    fileName[^1] = readChar;
+                }
+                Array.Resize(ref fileName, fileName.Length - 1);
+                subfile[i].Name = new(fileName);
+                br.BaseStream.Position = nameOffset + 0XF8;
+                subfile[i].Offset = br.ReadUInt32();
+                subfile[i].Size = br.ReadUInt32();
+            }
+
+            for (int i = 0; i < subfile.Length; i++)
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(source.Name) + "\\" + Path.GetFileNameWithoutExtension(source.Name));
+                BinaryWriter bw = new(File.OpenWrite(Path.GetDirectoryName(source.Name) + "\\" + Path.GetFileNameWithoutExtension(source.Name) + "\\" + subfile[i].Name));
+                br.BaseStream.Position = subfile[i].Offset;
+                bw.Write(br.ReadBytes((int)subfile[i].Size));
+
+                bw.Close();
             }
         }
     }
